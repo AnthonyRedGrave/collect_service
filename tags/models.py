4d1b4b2cf2f295d4d4962422c29c069a2779d977
wsa-045-23b <wsa-045-23b@wsa-045-23b.local>
2021-07-12 18:24:36 +0300

    add tag model, add field tags to thing

--- tags/models.py
+++ tags/models.py
@@ -1,3 +1,11 @@
 from django.db import models
 
-# Create your models here.
+class Tag(models.Model):
+    title = models.CharField('Название тэга', max_length=150)
+
+    def __str__(self):
+        return self.title
+
+    class Meta:
+        verbose_name = 'Тэг'
+        verbose_name_plural = 'Тэги'
