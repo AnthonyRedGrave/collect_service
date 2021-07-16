ded402782146c58a24949836483b9b4bf9329b96
wsa-045-23b <wsa-045-23b@wsa-045-23b.local>
2021-07-16 15:57:59 +0300

    rename action method

--- things/admin.py
+++ things/admin.py
@@ -1,15 +1,16 @@
 from django.contrib import admin
 from .models import *
+import csv
 
 @admin.action(description='CSV-Import')
-def make_published(modeladmin, request, queryset):
+def csv_import(modeladmin, request, queryset):
     print(queryset)
 
 @admin.register(Thing)
 class ThingAdmin(admin.ModelAdmin):
     list_display = ('title', 'content', 'owner', 'state', 'section', 'is_sold', 'date_published')
     list_display_links = ('title', 'content', 'owner', 'state')
-    actions = (make_published,)
+    actions = (csv_import,)
 
 @admin.register(ThingMessage)
 class ThingMessageAdmin(admin.ModelAdmin):
