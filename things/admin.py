1b7c9d33422c333b0f163a68d781440337fe84fa
wsa-045-23b <wsa-045-23b@wsa-045-23b.local>
2021-07-16 15:46:45 +0300

    add csv-import action init

--- things/admin.py
+++ things/admin.py
@@ -1,12 +1,15 @@
 from django.contrib import admin
 from .models import *
 
+@admin.action(description='CSV-Import')
+def make_published(modeladmin, request, queryset):
+    print(queryset)
 
 @admin.register(Thing)
 class ThingAdmin(admin.ModelAdmin):
     list_display = ('title', 'content', 'owner', 'state', 'section', 'is_sold', 'date_published')
     list_display_links = ('title', 'content', 'owner', 'state')
-
+    actions = (make_published,)
 
 @admin.register(ThingMessage)
 class ThingMessageAdmin(admin.ModelAdmin):
