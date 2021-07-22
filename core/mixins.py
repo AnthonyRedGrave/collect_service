02cb71d26e31879c2e9bff913534ece178e04515
wsa-045-23b <wsa-045-23b@wsa-045-23b.local>
2021-07-22 14:12:05 +0300

    add manager for deleted objects

--- core/mixins.py
+++ core/mixins.py
@@ -1,8 +1,16 @@
 from django.db import models
 
 
+class DeletedObjectsManager(models.Manager):
+    def get_queryset(self):
+        return super().get_queryset().filter(deleted=True)
+
+
 class SoftDeleteMixin(models.Model):
     deleted = models.BooleanField("Удалено ли", default=False)
+    objects = models.Manager()
+    deleted_objects = DeletedObjectsManager()
+    
 
     def delete(object, *args, **kwargs):
         object.deleted = True
