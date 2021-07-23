44e6158076ce57e1939c7b297adcd0a01dde08ea
wsa-045-23b <wsa-045-23b@wsa-045-23b.local>
2021-07-23 16:09:29 +0300

    export with tags

--- things/services.py
+++ things/services.py
@@ -2,7 +2,7 @@ import csv
 from .models import Thing
 
 
-def csv_export_service(filename):
+def csv_export_service(filename = "export.csv"):
     queryset = Thing.objects.all()
     model = queryset.model
     opts = model._meta.fields + model._meta.many_to_many
@@ -10,7 +10,9 @@ def csv_export_service(filename):
     with open(f"media/csv-things/{filename}", "w") as f:
         writer = csv.writer(f, delimiter=";")
         field_names = [field.name for field in opts]
+        object_fields = ["id", "title","content", "state", "section", "date_published", "image", "is_sold", "owner"]
         writer.writerow(field_names)
         for obj in queryset:
-            writer.writerow([getattr(obj, field) for field in field_names])
-    return f
+            fields_list = [getattr(obj, field) for field in object_fields]
+            fields_list.append(list(obj.tags.values_list("title", flat=True)))
+            writer.writerow(fields_list)
