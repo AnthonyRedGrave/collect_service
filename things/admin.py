2a57541e5f6ac2762297c4947d325b8509142416
amate <obarnev@inbox.ru>
2021-07-25 15:56:31 +0300

    add tag_inline in admin edit thing

--- things/admin.py
+++ things/admin.py
@@ -1,19 +1,35 @@
 from django.contrib import admin
 from .models import *
+from tags.models import Tag
+
+
+class TagInline(admin.TabularInline):
+    model = Thing.tags.through
+    extra = 3
 
 
 @admin.register(Thing)
 class ThingAdmin(admin.ModelAdmin):
-    list_display = ('title', 'content', 'owner', 'state', 'section', 'is_sold', 'date_published')
-    list_display_links = ('title', 'content', 'owner', 'state')
+    list_display = (
+        "title",
+        "content",
+        "owner",
+        "state",
+        "section",
+        "is_sold",
+        "date_published",
+    )
+    list_display_links = ("title", "content", "owner", "state")
+    inlines = (TagInline,)
+    exclude = ('tags',)
 
 
 @admin.register(ThingMessage)
 class ThingMessageAdmin(admin.ModelAdmin):
-    list_display = ('user', 'content', 'thing')
-    list_display_links = ('user', 'content', 'thing')
+    list_display = ("user", "content", "thing")
+    list_display_links = ("user", "content", "thing")
 
 
 @admin.register(Section)
 class SectionAdmin(admin.ModelAdmin):
-    list_display = ('title',)
+    list_display = ("title",)
