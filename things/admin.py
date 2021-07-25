fa05e298e6c6f81e39dfa112d687bdc66e60d325
amate <obarnev@inbox.ru>
2021-07-25 16:07:22 +0300

    comment_inline in admin

--- things/admin.py
+++ things/admin.py
@@ -1,11 +1,20 @@
 from django.contrib import admin
 from .models import *
+from django.contrib.contenttypes.admin import GenericTabularInline
+
+from comments.models import Comment
+
+
+class CommentInline(GenericTabularInline):
+    model = Comment
+    extra = 3
 
 
 @admin.register(Thing)
 class ThingAdmin(admin.ModelAdmin):
     list_display = ('title', 'content', 'owner', 'state', 'section', 'is_sold', 'date_published')
     list_display_links = ('title', 'content', 'owner', 'state')
+    inlines = [CommentInline,]
 
 
 @admin.register(ThingMessage)
