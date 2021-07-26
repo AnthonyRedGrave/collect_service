f5fa49fa9176a3f5caee31f56c001b2edb6a5431
wsa-045-23b <wsa-045-23b@wsa-045-23b.local>
2021-07-26 11:19:57 +0300

    fix tests to create_thing_serializer, add new

--- things/tests/serializers/test_create_thing_serializer.py
+++ things/tests/serializers/test_create_thing_serializer.py
@@ -7,132 +7,160 @@ from rest_framework.validators import ValidationError
 
 pytestmark = pytest.mark.django_db
 
-@pytest.mark.parametrize("title", ("Монетка", "  Фантик  ", "мОНетКа", 123))
-def test_create_thing_serializer_title__success(title):
+
+@pytest.mark.parametrize("tags", (["1", "2", "3"], [True, False, True], [0, 0, 0]))
+def test_create_thing_serializer_tags__error(tags):
     thing = ThingFactory()
     data = {
-        'title': title, 
-        'state': thing.state, 
-        'owner': thing.owner.id, 
-        'content': thing.content, 
-        'section': thing.section.id
+        "title": thing.title,
+        "state": thing.state,
+        "owner": thing.owner.id,
+        "content": thing.content,
+        "section": thing.section.id,
+        "tags": tags,
+    }
+    serializer = CreateThingSerializer(data=data)
+    with pytest.raises(ValidationError):
+        serializer.is_valid(raise_exception=True)
+    assert "tags" in serializer.errors.keys()
+
+
+@pytest.mark.parametrize("title", ("Монетка", "  Фантик  ", "мОНетКа", 123))
+def test_create_thing_serializer_title__success(title):
+    thing = ThingFactory(tags=4)
+    data = {
+        "title": title,
+        "state": thing.state,
+        "owner": thing.owner.id,
+        "content": thing.content,
+        "section": thing.section.id,
+        "tags": list(thing.tags.values_list("id", flat=True)),
     }
     serializer = CreateThingSerializer(data=data)
     assert serializer.is_valid()
     assert serializer.data == {
-                                'title': str(title).strip(), 
-                                'state': thing.state,
-                                'owner': thing.owner.id, 
-                                'image': None,
-                                'content': thing.content, 
-                                'section': thing.section.id,
-                              }
+        "title": str(title).strip(),
+        "state": thing.state,
+        "owner": thing.owner.id,
+        "image": None,
+        "content": thing.content,
+        "section": thing.section.id,
+        "tags": list(thing.tags.values_list("id", flat=True)),
+    }
     assert serializer.errors == {}
 
 
 @pytest.mark.parametrize("state", ("Awesome", "Good", "Shabby"))
 def test_create_thing_serializer_state__success(state):
-    thing = ThingFactory()
+    thing = ThingFactory(tags=4)
     data = {
-        'title': thing.title, 
-        'state': state, 
-        'owner': thing.owner.id, 
-        'content': thing.content, 
-        'section': thing.section.id
+        "title": thing.title,
+        "state": state,
+        "owner": thing.owner.id,
+        "content": thing.content,
+        "section": thing.section.id,
+        "tags": list(thing.tags.values_list("id", flat=True)),
     }
     serializer = CreateThingSerializer(data=data)
     assert serializer.is_valid()
     assert serializer.data == {
-                                'title': thing.title, 
-                                'state': state.strip(),
-                                'owner': thing.owner.id, 
-                                'image': None,
-                                'content': thing.content, 
-                                'section': thing.section.id,
-                              }
+        "title": thing.title,
+        "state": state.strip(),
+        "owner": thing.owner.id,
+        "image": None,
+        "content": thing.content,
+        "section": thing.section.id,
+        "tags": list(thing.tags.values_list("id", flat=True)),
+    }
     assert serializer.errors == {}
 
 
 @pytest.mark.parametrize("title", (None, False, ""))
 def test_create_thing_serializer_title__error(title):
-    thing = ThingFactory()
+    thing = ThingFactory(tags=4)
     data = {
-        'title': title, 
-        'state': thing.state, 
-        'owner': thing.owner.id, 
-        'content': thing.content, 
-        'section': thing.section.id
+        "title": title,
+        "state": thing.state,
+        "owner": thing.owner.id,
+        "content": thing.content,
+        "section": thing.section.id,
+        "tags": list(thing.tags.values_list("id", flat=True)),
     }
     serializer = CreateThingSerializer(data=data)
 
     with pytest.raises(ValidationError):
         serializer.is_valid(raise_exception=True)
-    assert 'title' in serializer.errors.keys()
-    assert 'state' not in serializer.errors.keys()
-    assert 'content' not in serializer.errors.keys()
-    assert 'section' not in serializer.errors.keys()
-    assert 'owner' not in serializer.errors.keys()
+    assert "title" in serializer.errors.keys()
+    assert "state" not in serializer.errors.keys()
+    assert "content" not in serializer.errors.keys()
+    assert "section" not in serializer.errors.keys()
+    assert "owner" not in serializer.errors.keys()
 
 
 @pytest.mark.parametrize("state", ("Not valid state", "", "  Awesome  "))
 def test_create_thing_serializer_state__error(state):
     thing = ThingFactory()
     data = {
-        'title': thing.title, 
-        'state': state, 
-        'owner': thing.owner.id, 
-        'content': thing.content, 
-        'section': thing.section.id
+        "title": thing.title,
+        "state": state,
+        "owner": thing.owner.id,
+        "content": thing.content,
+        "section": thing.section.id,
+        "tags": list(thing.tags.values_list("id", flat=True)),
     }
     serializer = CreateThingSerializer(data=data)
 
     with pytest.raises(ValidationError):
         serializer.is_valid(raise_exception=True)
-    assert 'state' in serializer.errors.keys()
-    assert 'title' not in serializer.errors.keys()
-    assert 'content' not in serializer.errors.keys()
-    assert 'section' not in serializer.errors.keys()
-    assert 'owner' not in serializer.errors.keys()
+    assert "state" in serializer.errors.keys()
+    assert "title" not in serializer.errors.keys()
+    assert "content" not in serializer.errors.keys()
+    assert "section" not in serializer.errors.keys()
+    assert "owner" not in serializer.errors.keys()
 
 
 @pytest.mark.parametrize("content", ("Content1", "   Content2   ", 12345689))
 def test_create_thing_serializer_content__success(content):
-    thing = ThingFactory()
+    thing = ThingFactory(tags=4)
     data = {
-        'title': "Монетка 16-го века",
-        'state': "Awesome",
-        'owner': thing.owner.id, 
-        'content': content, 
-        'section': thing.section.id
+        "title": "Монетка 16-го века",
+        "state": "Awesome",
+        "owner": thing.owner.id,
+        "content": content,
+        "section": thing.section.id,
+        "tags": list(thing.tags.values_list("id", flat=True)),
     }
     serializer = CreateThingSerializer(data=data)
-    
+
     assert serializer.is_valid()
     assert serializer.data == {
-                                'title': "Монетка 16-го века",
-                                'state': "Awesome",
-                                'content': str(content).strip(),
-                                'owner': thing.owner.id, 
-                                'image': None,
-                                'section': thing.section.id,
+        "title": "Монетка 16-го века",
+        "state": "Awesome",
+        "content": str(content).strip(),
+        "owner": thing.owner.id,
+        "image": None,
+        "section": thing.section.id,
+        "tags": list(thing.tags.values_list("id", flat=True)),
     }
     assert serializer.errors == {}
 
+
 @pytest.mark.parametrize("content", ("", None, False))
 def test_create_thing_serializer_content__error(content):
-    thing = ThingFactory()
+    thing = ThingFactory(tags=4)
     data = {
-        'title': "Монетка 16-го века",
-        'state': "Awesome",
-        'owner': thing.owner.id, 
-        'content': content, 
-        'section': thing.section.id
+        "title": "Монетка 16-го века",
+        "state": "Awesome",
+        "owner": thing.owner.id,
+        "content": content,
+        "section": thing.section.id,
+        "tags": list(thing.tags.values_list("id", flat=True)),
     }
     serializer = CreateThingSerializer(data=data)
     with pytest.raises(ValidationError):
         serializer.is_valid(raise_exception=True)
-    assert 'content' in serializer.errors.keys()
-    assert 'title' not in serializer.errors.keys()
-    assert 'state' not in serializer.errors.keys()
-    assert 'section' not in serializer.errors.keys()
-    assert 'owner' not in serializer.errors.keys()
+    assert "content" in serializer.errors.keys()
+    assert "title" not in serializer.errors.keys()
+    assert "state" not in serializer.errors.keys()
+    assert "section" not in serializer.errors.keys()
+    assert "owner" not in serializer.errors.keys()
