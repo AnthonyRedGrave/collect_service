84fe1e8328354d03775d100f6faba9e237d136b0
Oleg <obarnev@inbox.ru>
2021-07-30 16:05:19 +0300

    add context data to activateemail

--- things/email.py
+++ things/email.py
@@ -3,3 +3,8 @@ from djoser import email
 
 class ActivationEmail(email.ActivationEmail):
     template_name = 'email/activation.html'
+
+    def get_context_data(self, *args, **kwargs):
+        context = super().get_context_data(**kwargs)
+        context['var'] = "Hello world"
+        return context
